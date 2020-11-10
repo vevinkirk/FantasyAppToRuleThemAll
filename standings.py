42678f980463a17df959b3c3fc9cd98186484714
Kevin Virk <13743689+vevinkirk@users.noreply.github.com>
2020-11-09 16:01:54 -0800

    Update standings.py
    remove typo

--- standings.py
+++ standings.py
@@ -7,10 +7,6 @@ Using Sportsdata.io this is a python3 example on how to get standings. We can us
 I currently have one but omitted it here for security reasons.
 '''
 
-import os
-import requests
-import json
-
 def get_standings():
     url = 'https://api.sportsdata.io/v3/nfl/scores/json/Standings/2020'
     r = requests.get(url, headers={'Ocp-Apim-Subscription-Key': '%s' % 'API_KEY'})
