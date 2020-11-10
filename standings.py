30d9d5e1e68397d3f631e133f6c1c2c16ed8a22b
Kevin Virk <13743689+vevinkirk@users.noreply.github.com>
2020-11-09 16:01:42 -0800

    Update standings.py
    Output of standings is in better format and can be parsed easier

--- standings.py
+++ standings.py
@@ -7,8 +7,19 @@ Using Sportsdata.io this is a python3 example on how to get standings. We can us
 I currently have one but omitted it here for security reasons.
 '''
 
+import os
+import requests
+import json
+
 def get_standings():
     url = 'https://api.sportsdata.io/v3/nfl/scores/json/Standings/2020'
     r = requests.get(url, headers={'Ocp-Apim-Subscription-Key': '%s' % 'API_KEY'})
     standings = r.json()
-    print(standings)
+    print(len(standings))
+    print("Print each key-value pair from JSON response")
+    for x in range(len(standings)):
+        #print(x)
+        for key, value in standings[x].items():
+            print(key, ":", value)
+
+get_standings()
